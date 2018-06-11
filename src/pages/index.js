8f3316d5a6f09d65e9011e0821046bd33b3060f5
Charles Pernet <charlespernet@gmail.com>
2018-06-11 18:02:35 +0200

    wip first section

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,20 @@ const IndexPage = () => (
         <p>Tarifs</p>
       </div>
     </nav>
+
+    <h1 className="p-4">
+      Ne vous retrouvez plus jamais <br/>
+      coincés devant la porte.
+    </h1>
+    <div className="nav-button p-3">
+      <button className="btn btn-lg btn-gray px-4">
+        Sonnez le portier
+      </button>
+    </div>
+    <h2 className="pt-2">
+      Un numéro unique pour déposer et récupérer vos clés <br/>
+      si vous vous êtes enfermé(e) dehors
+    </h2>
   </div>
 )
 

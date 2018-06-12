180def697f71c24f1130c537c11e544da6d99160
Charles Pernet <charlespernet@gmail.com>
2018-06-12 11:27:29 +0200

    Add color property to title to manage underline

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -4,10 +4,15 @@ import './title.scss'
 
 class Title extends Component {
   render() {
+    var undelineClass = classNames(
+      'title-underline',
+      this.props.color
+    );
+
     return (
       <div className="title text-center py-4">
-        <h2 className="title-text">{props.text}</h2>
-        <div className="title-underline"></div>
+        <h2 className="title-text">{this.props.text}</h2>
+        <div className={undelineClass}></div>
       </div>
     )
   }

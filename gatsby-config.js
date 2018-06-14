043386f51a4cdb60cf9bf553d5edabfaffd53976
Charles Pernet <charlespernet@gmail.com>
2018-06-14 10:57:53 +0200

    Add gatsby-plugin-favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,24 @@ module.exports = {
           `GFS Didot`
         ]
       }
+    },
+    {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/images/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
     }
   ]
 }

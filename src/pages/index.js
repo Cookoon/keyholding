a52ef8017f4cead4344415f36763916973b123e6
Francois Catuhe <fcatuhe@gmail.com>
2018-06-25 12:06:05 +0200

    prettify index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,31 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import Helmet from 'react-helmet';
-import Title from '../components/title';
-import logo from '../images/logo.png';
-import logoCookoon from '../images/logo-cookoon.png';
-import competitif from '../images/ico-competitif.svg';
-import efficace from '../images/ico-efficace.svg';
-import fiable from '../images/ico-fiable.svg';
-import simple from '../images/ico-simple.svg';
+import React from 'react'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import Title from '../components/title'
+import logo from '../images/logo.png'
+import logoCookoon from '../images/logo-cookoon.png'
+import competitif from '../images/ico-competitif.svg'
+import efficace from '../images/ico-efficace.svg'
+import fiable from '../images/ico-fiable.svg'
+import simple from '../images/ico-simple.svg'
 
 const IndexPage = () => (
   <div>
     <div className="presentation">
       <div className="container">
-        <nav className='d-flex'>
+        <nav className="d-flex">
           <div className="d-flex justify-content-between align-items-end logo-block">
-            <img src={logo} alt="" id='logo'/>
-            <p className='d-none d-md-block'>
-              <span className="logo-text">Le portier</span><br />
+            <img src={logo} alt="" id="logo" />
+            <p className="d-none d-md-block">
+              <span className="logo-text">Le portier</span>
+              <br />
               <span className="small">par Cookoon</span>
             </p>
           </div>
           <div className="nav-text pt-3 text-center">
-            <Link to="#engagement">
-              Le Service
-            </Link>
-            <Link to="#explanation">
-              Offres
-            </Link>
-            <Link to="#pricing">
-              Tarifs
-            </Link>
+            <Link to="#engagement">Le Service</Link>
+            <Link to="#explanation">Offres</Link>
+            <Link to="#pricing">Tarifs</Link>
           </div>
         </nav>
       </div>
@@ -40,24 +35,23 @@ const IndexPage = () => (
           Des invités arrivés en avance ? <br />
         </h1>
         <div className="nav-button">
-          <a href='https://cookoon.typeform.com/to/yDXnaq'>
-            <button className="btn btn-custom">
-              S'inscrire
-            </button>
+          <a href="https://cookoon.typeform.com/to/yDXnaq">
+            <button className="btn btn-custom">S'inscrire</button>
           </a>
         </div>
         <h3 className="pt-4">
-          Le portier vous livre un double de vos clefs, en deux heures, 24H/24 7J/7
+          Le portier vous livre un double de vos clefs, en deux heures, 24H/24
+          7J/7
         </h3>
       </div>
     </div>
-    <div className="engagement" id='engagement'>
+    <div className="engagement" id="engagement">
       <Title text="La promesse du portier" />
       <div className="container py-4">
         <div className="row">
           <div className="col-md pb-4 px-4">
             <div className="d-flex">
-              <img src={simple} alt="simple" className='bullets'/>
+              <img src={simple} alt="simple" className="bullets" />
               <div className="content pl-3">
                 <h3>SIMPLE</h3>
                 <p>
@@ -69,7 +63,7 @@ const IndexPage = () => (
           </div>
           <div className="col-md pb-4 px-4">
             <div className="d-flex">
-              <img src={efficace} alt="efficace" className='bullets'/>
+              <img src={efficace} alt="efficace" className="bullets" />
               <div className="content pl-3">
                 <h3>EFFICACE</h3>
                 <p>
@@ -83,7 +77,7 @@ const IndexPage = () => (
         <div className="row">
           <div className="col-md pb-4 px-4">
             <div className="d-flex">
-              <img src={competitif} alt="competitif" className='bullets'/>
+              <img src={competitif} alt="competitif" className="bullets" />
               <div className="content pl-3">
                 <h3>COMPÉTITIF</h3>
                 <p>
@@ -95,7 +89,7 @@ const IndexPage = () => (
           </div>
           <div className="col-md pb-4 px-4">
             <div className="d-flex">
-              <img src={fiable} alt="fiable" className='bullets'/>
+              <img src={fiable} alt="fiable" className="bullets" />
               <div className="content pl-3">
                 <h3>SÉCURISÉ</h3>
                 <p>
@@ -107,8 +101,8 @@ const IndexPage = () => (
         </div>
       </div>
     </div>
-    <div className="explanation" id='explanation'>
-      <Title text="Notice d'emploi:" color="white"/>
+    <div className="explanation" id="explanation">
+      <Title text="Notice d'emploi:" color="white" />
 
       <div className="container">
         <div className="row text-center">
@@ -122,9 +116,7 @@ const IndexPage = () => (
           </div>
           <div className="col-md">
             <h3>2.</h3>
-            <p>
-              Vos clefs sont stockées dans un coffre fort.
-            </p>
+            <p>Vos clefs sont stockées dans un coffre fort.</p>
           </div>
           <div className="col-md">
             <h3>3.</h3>
@@ -137,7 +129,7 @@ const IndexPage = () => (
         </div>
       </div>
     </div>
-    <div className="pricing" id='pricing'>
+    <div className="pricing" id="pricing">
       <Title text="Les tarifs" />
       <div className="container">
         <div className="bubbles row">
@@ -145,18 +137,20 @@ const IndexPage = () => (
             <div className="bubble-container">
               <div className="bubble-black text-center">
                 <div>
-                  <span className='font-weight-bold'>2,90€</span>
+                  <span className="font-weight-bold">2,90€</span>
                   <br />/ mois
                 </div>
               </div>
               <div className="bubble-yellow left text-center">
                 <div>
-                  <span className='font-weight-bold'>24,90 €</span>
+                  <span className="font-weight-bold">24,90 €</span>
                   <br />/ an
                 </div>
               </div>
               <div className="bubble-text-left">
-                <p className='text-center'>Récolte & Gardiennage<br /> de vos clés</p>
+                <p className="text-center">
+                  Récolte & Gardiennage<br /> de vos clés
+                </p>
               </div>
             </div>
           </div>
@@ -164,18 +158,20 @@ const IndexPage = () => (
             <div className="bubble-container">
               <div className="bubble-black text-center">
                 <div>
-                  <span className='font-weight-bold'>30 €</span>
+                  <span className="font-weight-bold">30 €</span>
                   <br />8h-20h
                 </div>
               </div>
               <div className="bubble-yellow right text-center">
                 <div>
-                  <span className='font-weight-bold'>45 €</span>
+                  <span className="font-weight-bold">45 €</span>
                   <br />8h-20h
                 </div>
               </div>
               <div className="bubble-text-right">
-                <p className='text-center'>Livraison à domicile<br /> de vos clés</p>
+                <p className="text-center">
+                  Livraison à domicile<br /> de vos clés
+                </p>
               </div>
             </div>
           </div>
@@ -183,12 +179,9 @@ const IndexPage = () => (
       </div>
     </div>
 
-
     <div className="cta text-center">
-      <a href='https://cookoon.typeform.com/to/yDXnaq'>
-        <button className="btn btn-custom">
-          S'inscrire
-        </button>
+      <a href="https://cookoon.typeform.com/to/yDXnaq">
+        <button className="btn btn-custom">S'inscrire</button>
       </a>
     </div>
     <footer className="py-4 px-2">
@@ -202,8 +195,10 @@ const IndexPage = () => (
               <p>Contact</p>
               <p>Conditions Générales</p>
               <div className="d-flex align-items-start">
-                <p>by <span>Cookoon</span></p>
-                <img src={logoCookoon} alt="Logo Cookoon"/>
+                <p>
+                  by <span>Cookoon</span>
+                </p>
+                <img src={logoCookoon} alt="Logo Cookoon" />
               </div>
             </div>
           </div>
